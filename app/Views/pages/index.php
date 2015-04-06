bec5e7727bda37ce052a37117c856b004e0f1dd0
Ian Channing <ian@klever.co.uk>
2015-04-06 04:53:40 +0200

    Improvement, a little bit of love 1. In layouts/default.php Format the head to closer match HTML 5 Boiler Plate 2. In pages/index.php style the H1 tag to add some jazz to the Times New Roman

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,1 +1,12 @@
+<style type="text/css">
+    h1 {
+        border-bottom: 1px solid #EEE;
+        color: #666;
+        font-family: 'Times New Roman';
+        font-size: 64px;
+        font-weight: normal;
+        letter-spacing: -0.04em;
+        text-align: center;
+    };        
+</style>
 <h1><?php _e('Welcome to Vanda PHP'); ?></h1>
